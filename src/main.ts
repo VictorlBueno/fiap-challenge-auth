02dfcfc54517bb7d703edd76118d8485f1d21c29
Victor <victorlzbu@gmail.com>
2025-05-19 22:15:59 -0300

    chore: adjust cached server

--- src/main.ts
+++ src/main.ts
@@ -7,33 +7,32 @@ import * as serverless from 'serverless-http';
 let cachedServer: serverless.Handler;
 
 async function bootstrap(): Promise<any> {
-    if (!cachedServer) {
-        const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-        const config = new DocumentBuilder()
-            .setTitle('Auth API')
-            .setVersion('1.0')
-            .build();
+    const config = new DocumentBuilder()
+        .setTitle('Auth API')
+        .setVersion('1.0')
+        .build();
 
-        const document = SwaggerModule.createDocument(app, config);
-        SwaggerModule.setup('api', app, document, {
-            swaggerOptions: {
-                persistAuthorization: true,
-                supportedSubmitMethods: ['get', 'put', 'post', 'delete', 'options', 'head', 'patch', 'trace'],
-                displayRequestDuration: true,
-                docExpansion: 'list',
-                filter: true,
-                showExtensions: true,
-                showCommonExtensions: true,
-                tryItOutEnabled: true,
-            },
-            customSiteTitle: 'Fast Food Auth API Docs',
-        });
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document, {
+        swaggerOptions: {
+            persistAuthorization: true,
+            supportedSubmitMethods: ['get', 'put', 'post', 'delete', 'options', 'head', 'patch', 'trace'],
+            displayRequestDuration: true,
+            docExpansion: 'list',
+            filter: true,
+            showExtensions: true,
+            showCommonExtensions: true,
+            tryItOutEnabled: true,
+        },
+        customSiteTitle: 'Fast Food Auth API Docs',
+    });
 
-        app.useGlobalFilters(new GlobalExceptionFilter());
-        await app.init();
-        cachedServer = serverless(app.getHttpAdapter().getInstance());
-    }
+    app.useGlobalFilters(new GlobalExceptionFilter());
+
+    await app.init();
+    cachedServer = serverless(app.getHttpAdapter().getInstance());
     return cachedServer;
 }
 
